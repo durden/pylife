b40c11016925e954d85106418f8011a74f3ed805
Luke Lee <durdenmisc@gmail.com>
2010-07-11 11:30:47 -0500

    Remove last pygame dependencies into graphics library, separate out option parsing in driver program

--- life_game.py
+++ life_game.py
@@ -4,36 +4,52 @@
 Small driver program to simulate a game of Conway's life.
 """
 
-import sys
 import time
-import pygame
+from optparse import OptionParser
 
 from game import Game
-from pygame.locals import QUIT
 
 
-def run(game):
-    """Run through game of life simulation"""
+def run(game, secs):
+    """Run through game of life simulation, sleeping secs between
+       generations
+    """
     while True:
-        # FIXME: Catch keyboard
-        time.sleep(1)
-
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                return
-
+        time.sleep(secs)
         game.advance_generation()
 
 
+def get_args():
+    """Setup arguments"""
+    parser = OptionParser()
+
+    parser.add_option("-f", "--file", dest="filename",
+                        help="File to populate initial alive cells",
+                        metavar="FILE", default=None)
+
+    parser.add_option("-t", "--secs", dest="secs",
+                        help="Time (in seconds) between generations "
+                        "(decimals allowed)", default=1, type="float")
+
+    (options, args) = parser.parse_args()
+    return options
+
 def main():
     """Main entry point for driver program"""
-    filename = None
-    if len(sys.argv) > 1:
-        filename = sys.argv[1]
 
-    game = Game(640, 480, 4, filename)
-    run(game)
-    game.stop()
+    options = get_args()
+
+    # Python 2.4 doesn't allow except and finally clause together, so must
+    # wrap with outer finally instead
+    try:
+        # Run the game and always allow library to cleanup when user exits
+        try:
+            game = Game(640, 480, 4, options.filename)
+            run(game, options.secs)
+        except KeyboardInterrupt:
+            pass
+    finally:
+        game.stop()
 
 if __name__ == "__main__":
     main()
